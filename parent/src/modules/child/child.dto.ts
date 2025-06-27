0ad96717db7a420430a4051618ec97ba23ed2064
Abrar-sahib <abrarjamil906@gmail.com>
2025-06-27 12:50:06 +0500

    parent added

---
+++ parent/src/modules/child/child.dto.ts
@@ -0,0 +1,15 @@
+export class CreateChildDto {
+  fullName: string;
+  gender: 'male' | 'female';
+  grade: string;
+  age: number;
+  schoolName: string;
+}
+
+export class UpdateChildDto extends CreateChildDto {
+  id: string;
+}
+
+export class DeleteChildDto {
+  id: string;
+} 
