606f55b4cffaf1f0c8eff2638e9e82d68300d6fb
abrar_sab <abrarjamil906@gmail.com>
2026-03-13 16:06:44 +0500

    updates

--- ustaad-admin/src/modules/admin/admin.controller.ts
+++ ustaad-admin/src/modules/admin/admin.controller.ts
@@ -434,7 +434,7 @@ export default class AdminController {
     try {
       const page = Number(req.query.page) || 1;
       const limit = Number(req.query.limit) || 20;
-      const userId = req.user!.userId;
+      const userId = req.user!.id;
 
       const data = await this.adminService.getNotifications(
         userId,
